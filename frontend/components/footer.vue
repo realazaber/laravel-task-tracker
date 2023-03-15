27c5ce6e4b000ad9d09eabba17e5a28da0fd5b48
swagoverlord <azcooldude8@gmail.com>
2023-03-15 22:17:46 +0100

    Update footer.vue

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -1,6 +1,15 @@
 <template>
-    <footer>
-        footer
+    <footer class="flex flex-row justify-between p-5">
+        <div>
+            <Logo :width="12" :height="24" />
+        </div>
+        <div class="flex flex-col text-right gap-3
+        ">
+            <nuxt-link to="/">Home</nuxt-link>
+            <nuxt-link to="/about">About</nuxt-link>
+            <nuxt-link to="/contact">Contact Us</nuxt-link>
+            <nuxt-link to="/login">Login</nuxt-link>
+        </div>
     </footer>
 </template>
 
