3dd1e3062105ff252792d3720e1a1c8e2edd3181
swagoverlord <azcooldude8@gmail.com>
2023-03-18 14:22:40 +0100

    New slogan

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="min-h-[100vh] w-full flex flex-row text-center items-center justify-center">
         <div class="w-1/3 text-left">        
-            <h1 class="text-thir text-5xl font-bold">Time to get things done</h1>
-            <p class="py-5 text-pri font-semibold">Get It Done will help you remember and complete tasks on time. 
+            <h1 class="text-pri text-4xl font-bold w-full">Achieve more, stress less with Get It Done</h1>
+            <p class="py-5 text-accent font-medium w-4/6">Get It Done will help you remember and complete tasks on time. 
                 With features like recurring tasks, colour coding, dark mode and more.</p>
             <div class="flex py-3 gap-3">
                 <nuxt-link class="border border-2 border-[#565454] rounded-md p-3 font-semibold" to="/about">Learn more</nuxt-link>
