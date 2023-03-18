c92a97e7ab0e0b302fb45057f2c2a7fc519571ce
swagoverlord <azcooldude8@gmail.com>
2023-03-18 14:22:18 +0100

    Base Login and Register form

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,11 +1,9 @@
 <template>
-    <form>
-        <label for="email">Email</label>
-        <input type="email" name="email" required>
-        <label for="password">Password</label>
-        <input type="password" name="password" required>
-        <input type="submit" value="Login">
+    <form class="flex flex-col">
+        <input placeholder="Email" type="email" name="email" required>
+        <input placeholder="Password" type="password" name="password" required>
+        <input class="hover:drop-shadow-xl" type="submit" value="Login">
+        <nuxt-link class="pt-3" to="/register">Create account</nuxt-link>
     </form>
-    <nuxt-link to="/register">Create account</nuxt-link>
 </template>
 
--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,14 +1,10 @@
 <template>
-    <form>
-        <label for="name">Name</label>
-        <input type="text" name="name" required>
-        <label for="email">Email</label>
-        <input type="email" name="email" required>
-        <label for="con_password">Confirm password</label>
-        <input type="password" name="con_password" required>
-        <label for="password">Password</label>
-        <input type="password" name="password" required>
-        <input type="submit" value="Register">
-    </form>
+        <form class="flex flex-col p-5">
+            <input placeholder="Name" class="active:border-pri active:border-b-4" type="text" name="name" required>
+            <input placeholder="Email" type="email" name="email" required>
+            <input placeholder="Password" type="password" name="password" required>
+            <input placeholder="Confirm password" type="password" name="con_password" required>
+            <input type="submit" value="Register">
+        </form>
 </template>
 
