a1d600b96f6d112b257f65ab40968b909fec06e0
swagoverlord <azcooldude8@gmail.com>
2023-03-19 20:46:20 +0100

    Footer automatically gets current year

--- frontend/components/universal/footer.vue
+++ frontend/components/universal/footer.vue
@@ -1,18 +1,27 @@
 <template>
     <footer class="flex flex-row justify-between p-5">
-        <div>
-            <UniversalLogo :width="12" :height="24" />
-        </div>
-        <div class="flex flex-col text-right gap-3
-        ">
-            <nuxt-link to="/">Home</nuxt-link>
-            <nuxt-link to="/about">About</nuxt-link>
-            <nuxt-link to="/contact">Contact Us</nuxt-link>
-            <nuxt-link to="/login">Login</nuxt-link>
-        </div>
+      <div>
+        <UniversalLogo :width="12" :height="24" />
+      </div>
+      <div class="flex flex-col text-right gap-3">
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/about">About</nuxt-link>
+        <nuxt-link to="/contact">Contact Us</nuxt-link>
+        <nuxt-link to="/login">Login</nuxt-link>
+      </div>
     </footer>
     <div class="text-center bg-accent text-white p-1">
-        <a href="https://www.azaber.com" target="_blank">2023 Azaber</a>
+      <a href="https://www.azaber.com" target="_blank">© {{ year }} Azaber</a>
     </div>
-</template>
-
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        year: new Date().getFullYear()
+      };
+    }
+  };
+  </script>
+  
