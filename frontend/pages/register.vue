f8f42b3c7155c869ee2bab0e222f825102a0dbcf
swagoverlord <azcooldude8@gmail.com>
2023-03-19 19:42:04 +0100

    Title for register form

--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,5 +1,6 @@
 <template>
         <form class="flex flex-col p-5">
+            <h2 class="pb-5 text-xl text-center">Create Account</h2>
             <input placeholder="Name" class="focus:border-b-4 focus:border-b-sec focus:border-t-0" type="text" name="name" required>
             <input placeholder="Email" class="focus:border-b-4 focus:border-b-thir focus:border-t-0" type="email" name="email" required>
             <input placeholder="Password" class="focus:border-b-4 focus:border-b-pri focus:border-t-0" type="password" name="password" required>
