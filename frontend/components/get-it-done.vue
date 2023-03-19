9ba1b6b787beacd29022a63197b02efc353f2eb2
swagoverlord <azcooldude8@gmail.com>
2023-03-19 18:51:43 +0100

    Reusable component for the logo

---
+++ frontend/components/get-it-done.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="inline-block">
+    <div class="flex flex-row w-min">
+        <div class="text-thir">Get</div>
+        <div class="px-2 text-sec">It</div>
+        <div class="text-pri">Done</div>
+    </div>
+</div>
+</template>
