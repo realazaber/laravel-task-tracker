665467a4f15bbb38dc6878f2f0c5490abf37f7dc
swagoverlord <azcooldude8@gmail.com>
2023-03-19 18:51:56 +0100

    Content for about page

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        About
+        <p>
+            Are you tired of constantly feeling overwhelmed by your to-do list? Do you find yourself struggling to manage your busy schedule and stay on top of all your tasks? Look no further than "Get It Done" - the ultimate task tracking app designed to help you get things done efficiently and effectively.
+        </p>
+        <text-image paragraphText="One of the standout features is the sleek and stylish dark mode, which not only looks great but is also easier on your eyes, especially during those late night work sessions. With just a tap, you can switch to dark mode and continue working without any strain on your eyes." imageUrl="/images/svgs/dark_mode.svg" :textLeft="true"></text-image>
+        <text-image paragraphText="In addition to its stunning design, Get It Done offers a range of powerful features to help you manage your tasks with ease. One of the most useful is its recurring tasks feature, which allows you to set up recurring tasks and reminders so you never miss an important deadline or appointment again. With this feature, you can choose to repeat a task daily, weekly, monthly or yearly, making it easy to stay on top of your recurring responsibilities." imageUrl="/images/svgs/yellow/completed.svg" :textLeft="false"></text-image>
+        <text-image paragraphText="But that's not all - Get It Done is also soon to be available as a mobile app, giving you access to your to-do list on-the-go. Whether you're at work, at home, or on the move, you can easily manage your tasks and stay organized with the convenience of a mobile app." imageUrl="/images/svgs/red/my_app.svg" :textLeft="true"></text-image>
+        <p>
+            So why wait? Try "Get It Done" today and experience the power of a truly efficient and effective task tracker app. With its powerful features and sleek design, you'll wonder how you ever managed without it.
+        </p>
     </div>
 </template>
 
