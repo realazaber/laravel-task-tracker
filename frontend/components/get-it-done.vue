ebad5a3c0ad0749dadd90e3195604de6a4620a38
swagoverlord <azcooldude8@gmail.com>
2023-03-19 18:58:25 +0100

    Fixed text spacing

--- frontend/components/get-it-done.vue
+++ frontend/components/get-it-done.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="inline-block">
-    <div class="flex flex-row w-min">
+    <div class="flex flex-row w-min gap-1">
         <div class="text-thir">Get</div>
-        <div class="px-2 text-sec">It</div>
+        <div class="text-sec">It</div>
         <div class="text-pri">Done</div>
     </div>
 </div>
