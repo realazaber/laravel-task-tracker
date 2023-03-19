c204c4fb53f892ce511b770d466a6ce6f146326e
swagoverlord <azcooldude8@gmail.com>
2023-03-19 18:51:15 +0100

    Contact form connected to formspree

--- frontend/pages/contact.vue
+++ frontend/pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="flex flex-col">
+    <form action="https://formspree.io/f/mjvdevde" method="POST" class="flex flex-col">
         <h2 class="pb-5 text-xl text-center">Contact us</h2>
         <input placeholder="Name *" type="text" name="name" class="focus:border-b-4 focus:border-b-pri focus:border-t-0" required>
         <input placeholder="Email *" type="email" name="email" class="focus:border-b-4 focus:border-b-thir focus:border-t-0" required>
