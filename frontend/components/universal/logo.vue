fee1ffd07c97b43374a28e3b5902aaa68528dbe1
swagoverlord <azcooldude8@gmail.com>
2023-03-19 18:57:13 +0100

    Reusable component for the logo

---
+++ frontend/components/universal/logo.vue
@@ -0,0 +1,31 @@
+<template>
+    <nuxt-link to="/"><img :class="`${className}`" src="/images/logo.webp" alt="logo"></nuxt-link>
+</template>
+
+<script lang="ts">
+    let className = "";
+    export default {
+        props: {
+            height: {
+                type: Number,
+                default: 16
+            },
+            width: {
+                type: Number,
+                default: 24
+            },
+        },
+        computed: {
+            className() {
+                return `w-${this.width} h-${this.height}`;
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
