5379b559fdc790139626bcf047444baf3bc9b1b5
swagoverlord <azcooldude8@gmail.com>
2023-03-19 18:51:27 +0100

    Reusable component for about page

---
+++ frontend/components/text-image.vue
@@ -0,0 +1,45 @@
+<template>
+        <div :class="`flex my-5 mx-auto justify-center ${className}`">
+            <p class="w-2/3">
+                {{ paragraphText }}
+            </p>
+            <img :src="imageUrl" :alt="altText" class="w-1/3 h-96 rounded-md">
+        </div>
+</template>
+
+<script lang="ts">
+    export default {
+       props: {
+            paragraphText: {
+                type: String,
+                default: ""
+            },
+            altText: {
+                type: String,
+                default: "Get It Done"
+            },
+            imageUrl: {
+                type: String,
+                default: ""
+            },
+            textLeft: {
+                type: Boolean,
+                default: true
+            }
+       },
+       computed: {
+        className() {
+            if (this.textLeft == true) {
+                return `flex-row`
+            }
+            else {
+                return `flex-row-reverse`
+            }
+        }
+       } 
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
