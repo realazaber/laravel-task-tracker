7da3e289034f8a5a5ecb08809af1f4eba93aca3e
swagoverlord <azcooldude8@gmail.com>
2023-03-22 16:47:13 +0100

    Rewritten with the composition api

--- frontend/components/text-image.vue
+++ frontend/components/text-image.vue
@@ -1,48 +1,48 @@
 <template>
-        <div :class="`flex my-5 mx-auto justify-center ${className}`">
-            <p class="w-2/3">
-                {{ paragraphText }}
-            </p>
-            <div class="w-1/3">
-                <img :src="imageUrl" :alt="altText" class="h-96 rounded-xl object-top">
-            </div>
-            
-        </div>
-</template>
-
-<script lang="ts">
-    export default {
-       props: {
-            paragraphText: {
-                type: String,
-                default: ""
-            },
-            altText: {
-                type: String,
-                default: "Get It Done"
-            },
-            imageUrl: {
-                type: String,
-                default: ""
-            },
-            textLeft: {
-                type: Boolean,
-                default: true
-            }
-       },
-       computed: {
-        className() {
-            if (this.textLeft == true) {
-                return `flex-row`
-            }
-            else {
-                return `flex-row-reverse`
-            }
-        }
-       } 
-    }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+    <div :class="containerClass">
+      <div :class="`w-2/3 flex items-center mx-5 ${paragraphClass}`">
+        <p>{{ paragraphText }}</p>
+      </div>
+      <div class="w-1/3 flex" :class="imageClass">
+        <img :src="imageUrl" :alt="altText" class="h-96 rounded-xl object-top" />
+      </div>
+    </div>
+  </template>
+  <script>
+  import { computed } from 'vue';
+  
+  export default {
+    props: {
+      paragraphText: {
+        type: String,
+        default: '',
+      },
+      altText: {
+        type: String,
+        default: 'Get It Done',
+      },
+      imageUrl: {
+        type: String,
+        default: '',
+      },
+      textLeft: {
+        type: Boolean,
+        default: true,
+      },
+    },
+    setup(props) {
+      const containerClass = computed(() => `flex my-5 mx-auto justify-center items-center ${props.textLeft ? 'flex-row' : 'flex-row-reverse'}`);
+      const paragraphClass = computed(() => `w-1/2 ${props.textLeft ? 'justify-end' : 'justify-start'}`);
+      const imageClass = computed(() => `justify-${props.textLeft ? 'start' : 'end'}`);
+  
+      return {
+        containerClass,
+        paragraphClass,
+        imageClass,
+      };
+    },
+  };
+  </script>
+  <style lang="scss" scoped>
+  
+  </style>
