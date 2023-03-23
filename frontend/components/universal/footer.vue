c36810f219c6f815a73175d5a6504afed9e49099
swagoverlord <azcooldude8@gmail.com>
2023-03-23 15:06:23 +0100

    Added link to pricing page

--- frontend/components/universal/footer.vue
+++ frontend/components/universal/footer.vue
@@ -6,6 +6,7 @@
       <div class="flex flex-col text-right gap-3">
         <nuxt-link to="/">Home</nuxt-link>
         <nuxt-link to="/about">About</nuxt-link>
+        <nuxt-link to="/pricing">Pricing</nuxt-link>
         <nuxt-link to="/contact">Contact Us</nuxt-link>
         <nuxt-link to="/login">Login</nuxt-link>
       </div>
--- frontend/components/universal/nav.vue
+++ frontend/components/universal/nav.vue
@@ -3,6 +3,7 @@
         <UniversalLogo :width="24" :height="12" />
         <div class="flex flex-row gap-8 items-center">
             <nuxt-link class="hover:text-gray-900" to="/about">About</nuxt-link>
+            <nuxt-link class="hover:text-gray-900" to="/pricing">Pricing</nuxt-link>
             <nuxt-link class="hover:text-gray-900" to="/contact">Contact Us</nuxt-link>
             <nuxt-link class="border border-2 bg-pri border-pri text-sec font-semibold rounded-md hover:rounded-lg active:rounded-xl w-20 h-12 flex justify-center items-center" to="/login">Login</nuxt-link>
         </div>
