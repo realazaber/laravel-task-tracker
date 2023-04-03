6bb76017f796331a30e81be76c4f9d1d6e90441c
swagoverlord <azcooldude8@gmail.com>
2023-04-03 23:00:43 +0200

    responsive home page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="min-h-[100vh] w-full flex flex-row text-center items-center justify-center">
-        <div class="w-1/3 text-left">        
+    <div class="min-h-[100vh] w-full flex flex-col phone:flex-row text-center items-center justify-center">
+        <div class="w-full phone:w-1/3 text-left">        
             <h1 class="text-pri text-4xl font-bold w-full">
                 Achieve more<br />
                 Stress less <br />
@@ -11,12 +11,12 @@
                 <GetItDone></GetItDone>
                 will help you remember and complete tasks on time. 
                 With features like recurring tasks, colour coding, dark mode and more.</p>
-            <div class="flex py-3 gap-3">
-                <nuxt-link class="btn border border-2 border-[#565454] rounded-md p-3 font-semibold" to="/about">Learn more</nuxt-link>
-                <nuxt-link class="btn border border-2 border-sec bg-sec text-pri rounded-md p-3 font-semibold" to="/register">Get started</nuxt-link>
+            <div class="flex flex-col phone:flex-row py-3 gap-3">
+                <nuxt-link class="btn border border-2 border-thir text-thir rounded-md p-3 font-semibold" to="/about">Learn more</nuxt-link>
+                <nuxt-link class="btn border border-2 border-pri bg-pri text-white rounded-md p-3 font-semibold" to="/register">Get started</nuxt-link>
             </div>
         </div>
-        <div class="w-1/3">
+        <div class="w-2/3 phone:w-1/3">
             <img src="/images/svgs/red/placeholders.svg" alt="Welcome image">
         </div>
     </div>
